5a71259ed0bdbb261a45874d441009362942c7d1
Matheus Santos Antunes <sa.matheus2012@gmail.com>
2026-04-14 13:30:07 -0300

    aaa

--- api-usuarios/main.py
+++ api-usuarios/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
 
-    allow_host=:["5174"],
+    allow_origins=["http://localhost:5173"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
