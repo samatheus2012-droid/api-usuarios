f960e22219980e913b1e27caa2ff0b2302f6fb83
Matheus Santos Antunes <sa.matheus2012@gmail.com>
2026-04-14 14:10:35 -0300

    fix cors definitivo

--- api-usuarios/main.py
+++ api-usuarios/main.py
@@ -3,16 +3,22 @@ from pydantic import BaseModel
 import psycopg2
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+
+
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 DB_HOST = "ep-dark-paper-acegdo2q-pooler.sa-east-1.aws.neon.tech"
 DB_NAME = "neondb"
 DB_USER = "neondb_owner"
